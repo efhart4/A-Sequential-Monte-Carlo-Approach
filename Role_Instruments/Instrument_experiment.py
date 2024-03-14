a2c7dc0c71cefb565b0af226a4698eaa52ed7e12
efhart4 <84650748+efhart4@users.noreply.github.com>
2024-03-14 11:01:22 -0700

    Add files via upload

--- Role_Instruments/Instrument_experiment.py
+++ Role_Instruments/Instrument_experiment.py
@@ -656,11 +656,11 @@ def Metropolis_Hasting_Sampler_WO(y, x1, x2, true_theta):
             sample_paths[:, sample_count] = path_g
             sample_count += 1
     
-                # Print the acceptance rate every 100 draws
-                #if (count+1) % 500 == 0:
-                    #print("Acceptance rate:", accept_count / count) 
-                    # also print the count
-                    #print("Count:", count)
+        # Print the acceptance rate every 100 draws
+        #if (count+1) % 500 == 0:
+            #print("Acceptance rate:", accept_count / count) 
+            # also print the count
+            #print("Count:", count)
 
     return True, sample_paths, samples, uniform_priors, (accept_count / count)
 
@@ -823,17 +823,18 @@ def Metropolis_Hasting_Sampler_W(y, x1, x2, z, true_theta):
                     log_prior_g = log_prior_star
                     pY_theta_g = pY_theta_star
                     accept_count += 1
-
-                if count > burn_in:
-                    samples[:, sample_count] = theta_g
-                    sample_paths[:, sample_count] = path_g
-                    sample_count += 1
+        
+        # if prior is not finite, reject the draw and sample the old value if past burn in
+        if count > burn_in:
+            samples[:, sample_count] = theta_g
+            sample_paths[:, sample_count] = path_g
+            sample_count += 1
     
-                # Print the acceptance rate every 100 draws
-                #if (count+1) % 500 == 0:
-                    #print("Acceptance rate:", accept_count / count) 
-                    # also print the count
-                    #print("Count:", count)
+        # Print the acceptance rate every 100 draws
+        #if (count+1) % 500 == 0:
+            #print("Acceptance rate:", accept_count / count) 
+            # also print the count
+            #print("Count:", count)
 
     return True, sample_paths, samples, uniform_priors, (accept_count / count)
 
